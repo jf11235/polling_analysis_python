4bd98c9f12967c9299a4b8f872bb1ecc9fa1bac8
jf11235 <josh.h.feinberg@gmail.com>
2022-12-23 18:10:06 -0800

    working on changes in profit loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,11 @@ import csv
 
 date = []
 profloss = []
-budgetDict = {}
+changes = []
+#budgetDict = {}
+total = 0
+
+
 
 budget_csv = os.path.join("/Users/joshuafeinberg/Documents/U-of-O-Bootcamp/Bootcamp_Class_Folder/Module_Challenges/Module 3 Challange/PyBank/Resources/budget_data.csv")
 
@@ -13,13 +17,27 @@ with open(budget_csv) as csvfile:
     for row in csvreader:
         date.append(row[0])
         profloss.append(row[1])
+    date.pop(0)
+    profloss.pop(0)
     dataLen = len(date)
     print ("This data set has " + str(len(date)) + " months.") 
 
+    #print(date[0])
+    #print(profloss[0])
+    
+    for i in range(0, len(profloss)):
+        profloss[i] = int(profloss[i])
+        total = total + (profloss[i])
+        difference = (profloss[i+1]) - (profloss[i])
+        changes.append(difference)
+    print(changes[0])
+
+    #print(f' The total profit/loss is ${total}')
 
 
-    def difference(x,y):
-        return y-x
+
+
+    
 
     
 
